a87c4d4d299f38d8f898f502265f151550cfa4d1
Bernardo Pacini <bpacini@umich.edu>
2022-09-01 23:01:25 -0400

    Moving to dash instead of underscore

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='sphinx_ReadTheRecipe_theme',
+    name='sphinx-ReadTheRecipe-theme',
     version='0.0.1',
     url='https://github.com/bernardopacini/ReadTheRecipe',
     license='MIT',
     author='Bernardo Pacini',
     author_email='bpacini@umich.edu',
-    packages=['sphinx_ReadTheRecipe_theme'],
+    packages=['sphinx-ReadTheRecipe-theme'],
     include_package_data=True,
     install_requires=[
         'setuptools'
