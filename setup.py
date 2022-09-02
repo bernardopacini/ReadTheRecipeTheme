518e86273453e938c4925992d4680ce28ef27330
Bernardo Pacini <bpacini@umich.edu>
2022-09-01 23:03:25 -0400

    Removing dashes...

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='sphinx-ReadTheRecipe-theme',
+    name='sphinxReadTheRecipeTheme',
     version='0.0.1',
     url='https://github.com/bernardopacini/ReadTheRecipe',
     license='MIT',
     author='Bernardo Pacini',
     author_email='bpacini@umich.edu',
-    packages=['sphinx-ReadTheRecipe-theme'],
+    packages=['sphinxReadTheRecipeTheme'],
     include_package_data=True,
     install_requires=[
         'setuptools'
